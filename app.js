0d358df1003120dee3f6bee1e3d8db743025dcef
ceropack <88608780+ceropack@users.noreply.github.com>
2021-08-09 08:03:20 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+//const router = express.Router();
+const fs = require('fs');
+
+const phonic_o = fs.readFileSync('phonic_ex.json');
+const personObject = JSON.parse(phonic_o);
+/* let phonic_obj_prase = JSON.parse(phonic_obj); */
+
+app.set('view engine', 'ejs');
+app.get('/', function (req, res) {
+  res.render('index', { 
+    name: "Phonic",
+    items: personObject
+  })
+});
+
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!')
+});
